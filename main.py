d234c46e6301d12c46167dac3007456b761a4f5a
Edern76 <gawein.le@live.fr>
2017-02-08 16:07:56 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,69 +1,65 @@
-import libtcodpy as libtcod
-import xp_loader
+import tdl
+import xpLoaderPy3 as xpL
 import gzip
+import os
 
+offX, offY = 0, 0
+MOVEMENT_KEYS = {
+                 #Standard arrows
+                 'UP': [0, -1],
+                 'DOWN': [0, 1],
+                 'LEFT': [-1, 0],
+                 'RIGHT': [1, 0],
+                 }
+###################
+# Find the path to the file to display
+curDir = os.path.dirname(__file__)
+file = os.path.join(curDir, "xptest.xp")
+###################
+
+###################
+# Initialize the main console
+WIDTH, HEIGHT = 30, 30
+root = tdl.init(WIDTH, HEIGHT, 'XpLoaderPy3 Example')
+###################
+
+###################
+# Create a subconsole which will hold the image. It is definitely not mandatory to do so, but I did this just in order to show that it can be done.
+SUB_WIDTH, SUB_HEIGHT = 20, 20
+frame = tdl.Console(SUB_WIDTH, SUB_HEIGHT)
+###################
+
+###################
+# Get data from the .xp file
+unzippedFile = gzip.open(file).read() #Unzips the .xp file so we can call load_xp_string
+fileAttributes = xpL.load_xp_string(unzippedFile)
+fileWidth = fileAttributes["width"]
+fileHeight = fileAttributes["height"]
+layers = fileAttributes["layer_data"]
+###################
+
+###################
+# Main loop with display and input handling
+while not tdl.event.is_window_closed():
+	frame.clear()
+	root.clear()
+	root.draw_str(0, 0, "Width : {} / Height : {}".format(fileWidth, fileHeight), fg = (255, 0, 0), bg = Ellipsis)
+	for layer in layers:
+		xpL.load_layer_to_console(frame, layer, offX, offY)
+	root.blit(frame, 10, 10, SUB_WIDTH, SUB_HEIGHT, srcX=0, srcY=0)
+	tdl.flush()
+	key = tdl.event.key_wait()
+	if key.keychar.upper() in MOVEMENT_KEYS:
+		(x, y) = MOVEMENT_KEYS[key.keychar.upper()]
+		if offX + x in range(0, 10) and offY + y in range(0, 10):
+			offX += x
+			offY += y
+	elif key.keychar.upper() == "ESCAPE":
+		raise SystemExit("User pressed Escape")
 ####################
-# load in and read the .xp file - this is done out-of-library in case you have some other means to produce an uncompressed .xp data string
-####################
-
-xp_file = gzip.open('xptest.xp')
-raw_data = xp_file.read()
-xp_file.close()
-
-xp_data = xp_loader.load_xp_string(raw_data)
-
-screen_width = xp_data['width']
-screen_height = xp_data['height']
-limit_fps = 20
-
-####################
-# The default libtcod font sheet format is missing quite a few codepage 437 characters - if you want to use REXPaint, you'll need to find or make make a sprite sheet with those characters
-# Currently using the 10x10 default REXPaint sheet courtesy of Kyzrati
-####################
-
-libtcod.console_set_custom_font('cp437_10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_ASCII_INROW)
-libtcod.console_init_root(screen_width, screen_height, 'REXPaint Import Demo', False)
-libtcod.sys_set_fps(limit_fps)
-
-####################
-# Loads the layer data to offscreen consoles. You can load a layer to the main console by passing in 0 instead of a console
-####################
-
-layer_0_console = libtcod.console_new(xp_data['layer_data'][0]['width'],xp_data['layer_data'][0]['height'])
-layer_1_console = libtcod.console_new(xp_data['layer_data'][1]['width'],xp_data['layer_data'][1]['height'])
-
-xp_loader.load_layer_to_console(layer_0_console, xp_data['layer_data'][0])
-xp_loader.load_layer_to_console(layer_1_console, xp_data['layer_data'][1])
-
-####################
-# Sets the overlay layer transparency key to REXPaint's background transparency key. For completeness purposes, load_layer_to_console always writes every cell to the console - 
-# REXPaint format note - layer 0 background is written out as 0,0,0. If you're making .xp files for UI overlays or whatever, be absolutely sure to 
-####################
-
-libtcod.console_set_key_color(layer_1_console, libtcod.Color(xp_loader.transparent_cell_back_r, xp_loader.transparent_cell_back_g, xp_loader.transparent_cell_back_b))
-
-
-####################
-# libtcod piping to actually put the console layers on screen. This will probably change quite a bit for your actual usage of libtcod
-####################
-
-draw_layers = False
-
-while not libtcod.console_is_window_closed():
-
-	key = libtcod.console_check_for_keypress()
-	if key.vk == libtcod.KEY_ESCAPE:
-		break  #exit game
 	
-	if key.c == ord('a'):
-		draw_layers = not draw_layers
+	
+	 
 
-	libtcod.console_clear(0)
-	libtcod.console_blit(layer_0_console, 0, 0, xp_data['layer_data'][0]['width'], xp_data['layer_data'][0]['height'], 0,0, 0)
 
-	if draw_layers:
-		libtcod.console_blit(layer_1_console, 0, 0, xp_data['layer_data'][1]['width'], xp_data['layer_data'][1]['height'], 0, 0, 0)
-	libtcod.console_flush()
 
-libtcod.console_delete(layer_0_console)
-libtcod.console_delete(layer_1_console)
